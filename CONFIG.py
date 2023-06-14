29215564a0ee9b7db5e8556e74e330295ac16ae9
Nikita Lukyanov <119298712+NikitaLukyanov25@users.noreply.github.com>
2023-06-14 12:28:51 +0300

    Add files via upload

---
+++ CONFIG.py
@@ -0,0 +1,13 @@
+TOKEN = '6231929516:AAFvUm3fOjzzJjfnCv-XK2GRwOoo2VOJ7hc'
+
+keys = {
+    'рубль':"RUB",
+    'доллар':'USD',
+    'евро':'EUR',
+    'фунт':"GBP",
+    'биткоин':'BTC',
+    'эфир':"ETH",
+    "каспа":"KAS",
+
+
+}
